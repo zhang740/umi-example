0360efb76c81a61db3e0961996c5ff8a126eaddf
zhang740 <zhang740@vmzclub.com>
2018-01-12 15:06:08 +0800

    update: 调整title处理位置

--- components/base.js
+++ components/base.js
@@ -7,15 +7,21 @@ export class Component extends React.PureComponent {
   constructor(props, meta) {
     super(props);
 
-    // meta 处理
-    if (meta && meta.title) {
-      document.title = meta.title
-    }
+    this.meta = meta;
   }
 
   componentWillMount() {
     // init
     // http拦截器等
+  }
+
+  // 跟DOM有关的放到这里，防止SSR情况下异常
+  componentDidMount() {
+    // meta 处理
+    const meta = this.meta;
+    if (meta && meta.title) {
+      document.title = meta.title;
+    }
   }
 
   // 公共方法
